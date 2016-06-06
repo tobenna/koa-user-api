a3ccf596c6f01174c04d13bd57ca1678e67124ef
Tobenna <tobennandu@gmail.com>
2016-06-06 12:57:18 +0100

    Version for submitting

--- public/test/AppTest.js
+++
@@ -1,8 +0,0 @@
-import expect from 'expect';
-
-describe('Component: App', () => {
-  it('does all the things', () => {
-    expect(true).toEqual(true);
-  });
-  it('deletes itself when the "delete" button is clicked');
-});
